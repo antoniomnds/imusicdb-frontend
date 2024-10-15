573cee13908b5752bb6cad6d5fa870a35df1e370
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-10-15 23:32:04 +0100

    Fetch and display saved albums data

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,13 @@
-import {apiCall, API_GET} from "./utils/APIUtils.ts";
+import {apiCall, API_GET} from "./utils/APIUtils";
 
 export const API = {
     GetUser: async () => apiCall({
         method: API_GET,
         endpoint: "users/me"
+    }),
+    GetSavedAlbums: async (queryParams) => apiCall({
+        method: API_GET,
+        endpoint: "albums/me",
+        queryParams: queryParams
     })
 }
